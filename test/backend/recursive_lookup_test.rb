4a7a354f7b50afc6c1ac36eb32b51054e491d9a0
Markus Doits <markus.doits@stellenticket.de>
2016-10-04 16:31:12 +0200

    rubocop

--- test/backend/recursive_lookup_test.rb
+++ test/backend/recursive_lookup_test.rb
@@ -7,18 +7,19 @@ class I18nBackendRecursiveLookupTest < Test::Unit::TestCase
 
   def setup
     I18n.backend = Backend.new
-    I18n.backend.store_translations(:en,
-      :foo => 'foo',
-      :bar => {
-        :baz => 'bar ${foo}',
-        :boo => {
-          :baz => 'hoo ${bar.baz}'
+    I18n.backend.store_translations(
+      :en,
+      foo: 'foo',
+      bar: {
+        baz: 'bar ${foo}',
+        boo: {
+          baz: 'hoo ${bar.baz}'
         }
       },
-      :no_replacement => 'no replacement: $${foo}',
-      :alternate_lookup => '${baz}',
-      :hash_lookup => '${hash}',
-      :hash => {
+      no_replacement: 'no replacement: $${foo}',
+      alternate_lookup: '${baz}',
+      hash_lookup: '${hash}',
+      hash: {
         one: 'hash',
         other: 'hashes',
         deeper: {
@@ -26,11 +27,11 @@ class I18nBackendRecursiveLookupTest < Test::Unit::TestCase
           second: 'Second hash'
         }
       },
-      :number_hash => {
-        :format => {
-          :delimiter => ',',
-          :precision => 3,
-          :significant => false
+      number_hash: {
+        format: {
+          delimiter: ',',
+          precision: 3,
+          significant: false
         }
       }
     )
@@ -40,21 +41,21 @@ class I18nBackendRecursiveLookupTest < Test::Unit::TestCase
     I18n.backend = nil
   end
 
-  test "still returns an existing translation as usual" do
+  test 'still returns an existing translation as usual' do
     assert_equal 'foo', I18n.t(:foo)
   end
 
-  test "still fails for a missing key" do
-    assert_equal 'translation missing: en.missing_key', I18n.t(:'missing_key')
+  test 'still fails for a missing key' do
+    assert_equal 'translation missing: en.missing_key', I18n.t(:missing_key)
   end
 
-  test "does a lookup on an embedded key" do
+  test 'does a lookup on an embedded key' do
     assert_equal 'bar foo', I18n.t(:'bar.baz')
     assert_equal 'hoo bar foo', I18n.t(:'bar.boo.baz')
   end
 
-  test "does not transform $${...}" do
-    assert_equal 'no replacement: $${foo}', I18n.t(:'no_replacement')
+  test 'does not transform $${...}' do
+    assert_equal 'no replacement: $${foo}', I18n.t(:no_replacement)
   end
 
   # test "stores a compiled lookup" do
@@ -65,12 +66,13 @@ class I18nBackendRecursiveLookupTest < Test::Unit::TestCase
   #   assert_equal result, precompiled_result
   # end
 
-  test "resolves hash lookups" do
+  test 'resolves hash lookups' do
     assert_equal I18n.t(:'bar.boo').to_s, '{:baz=>"hoo bar foo"}'
   end
 
   test 'handles non-string results from lookup' do
-    assert_equal '{:delimiter=>",", :precision=>3, :significant=>false}', I18n.t(:'number_hash.format', :locale => :en, :default => {}).to_s
+    assert_equal '{:delimiter=>",", :precision=>3, :significant=>false}',
+                 I18n.t(:'number_hash.format', locale: :en, default: {}).to_s
   end
 
   # test "stores a compiled hash lookup" do
@@ -81,34 +83,34 @@ class I18nBackendRecursiveLookupTest < Test::Unit::TestCase
   #   assert_equal result, precompiled_result
   # end
 
-  test "correctly returns a hash" do
-    result = I18n.t(:'hash_lookup')
+  test 'correctly returns a hash' do
+    result = I18n.t(:hash_lookup)
 
     assert_equal Hash, result.class
 
     assert_equal({
-      one: 'hash',
-      other: 'hashes',
-      deeper: {
-        first: 'First hash',
-        second: 'Second hash'
-      }
-    }, result)
+                   one: 'hash',
+                   other: 'hashes',
+                   deeper: {
+                     first: 'First hash',
+                     second: 'Second hash'
+                   }
+                 }, result)
   end
 
-  test "correctly translates a hash reference with count" do
-    assert_equal 'hashes', I18n.t(:'hash_lookup', count: 5)
+  test 'correctly translates a hash reference with count' do
+    assert_equal 'hashes', I18n.t(:hash_lookup, count: 5)
   end
 
-  test "correctly translates a hash reference when called directly" do
+  test 'correctly translates a hash reference when called directly' do
     assert_equal 'hashes', I18n.t(:'hash_lookup.other')
   end
 
-  test "correctly translates a hash reference when called directly even when nested" do
+  test 'correctly translates a hash reference when called directly even when nested' do
     assert_equal 'Second hash', I18n.t(:'hash_lookup.deeper.second')
   end
 
-  test "correctly fails for a hash reference that is not present" do
+  test 'correctly fails for a hash reference that is not present' do
     assert_equal 'translation missing: en.hash_lookup.deeper.not_there.really',
                  I18n.t(:'hash_lookup.deeper.not_there.really')
   end
